bc095f42eb2174da8bf3545d98c26d996b3fd771
ainokomu <akomulainen@proton.me>
2026-01-29 10:59:15 +0200

    did first tests for title verification

---
+++ tests/unit/taskValidation.test.js
@@ -0,0 +1,24 @@
+//muuttuja, joka lloadaa moduulin
+const {validateTask} = require("../../src/tasks/taskService");
+
+/*
+Testit, joissa testataan onko title sopiva. Title ei voi puuttua, yllä tyhjä,
+olla vain whitespace. Oikealle titlelle ei tule virhettä
+*/
+describe("validateTask", ()=>{
+    test("throws if title is missing", () =>{
+        expect(()=> validateTask({})).toThrow(/title/i);
+    });
+
+    test("throws if title is empty string", () =>{
+        expect(()=> validateTask({title: ""})).toThrow(/title/i);
+    });
+
+    test("throws if title is only whitespace", ()=> {
+        expect(()=>validateTask({title:"   "})).toThrow(/title/i);
+    });
+
+    test("does not throw for a valid title", () =>{
+        expect(()=> validateTask({title:"Buy Milk"})).not.toThrow();
+    });
+});
