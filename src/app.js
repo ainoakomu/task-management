5bed65bbaa55521b932f95ef5f943f60a79e504b
Aino Komulainen <akomulainen@proton.me>
2026-02-12 19:14:49 +0200

    refactor on app post

--- src/app.js
+++ src/app.js
@@ -11,25 +11,24 @@ app.use(express.json());
 app.get("/health",(req,res)=>{
     res.json({status:"ok"});
 });
-app.post("/tasks",(req,res)=>{
+
+app.post("/tasks",(req,res,next)=>{
     console.log("BODY:", req.body);
     try{
         validateTask(req.body);
-        //yksinkertainen vastaus
         res.status(201).json(req.body);
     }catch(error){
-        res.status(400).json({error:error.message});
+        next(error);
     }
 });
 
 //error handling middleware
 app.use((err,req,res,next)=>{
     if(err.name==="ValidationError"){
-        res.status(400).json({error:err.message});
-    }else{
-        console.error(err);
-        res.status(500).json({error:"Internal Server Error"});
+         return res.status(400).json({error:err.message});
     }
+    return res.status(500).json({error:"Internal Server Error"});
+    
 });
 
 module.exports=app;
